804f4029fa07e18734164ac9fd92583a4b09e095
Daniel Wei <danielwei816@gmail.com>
2020-10-17 13:12:12 -0400

    Added firebase capability

--- Main.py
+++ Main.py
@@ -1,5 +1,85 @@
-<<<<<<< HEAD
-import pandas as pdimport numpy as npimport pandas_datareader as pdrimport matplotlib.pyplot as pltimport jsonimport tweepyfrom tweepy.streaming import StreamListenerfrom tweepy import OAuthHandlerfrom tweepy import Stream# import twitter keys and tokensfrom config import *data_amd = pdr.get_data_yahoo('AMD', '1-Feb-20')data_amd.head()data_amd['2_SMA'] = data_amd['Close'].rolling(window=2).mean()data_amd['5_SMA'] = data_amd['Close'].rolling(window=5).mean()data_amd = data_amd[data_amd['5_SMA'].notna()]# SMA trade callsTrade_Buy=[]Trade_Sell=[]for i in range(len(data_amd)-1):    if ((data_amd['2_SMA'].values[i] < data_amd['5_SMA'].values[i]) & (data_amd['2_SMA'].values[i+1] > data_amd['5_SMA'].values[i+1])):        print("Trade Call for {row} is Buy.".format(row=data_amd.index[i].date()))        Trade_Buy.append(i)    elif ((data_amd['2_SMA'].values[i] > data_amd['5_SMA'].values[i]) & (data_amd['2_SMA'].values[i+1] < data_amd['5_SMA'].values[i+1])):        print("Trade Call for {row} is Sell.".format(row=data_amd.index[i].date()))        Trade_Sell.append(i)plt.figure(figsize=(20, 10),dpi=80)plt.plot(data_amd.index, data_amd['Close'])plt.plot(data_amd.index, data_amd['2_SMA'],'-^', markevery=Trade_Buy, ms=15, color='green')plt.plot(data_amd.index, data_amd['5_SMA'],'-v', markevery=Trade_Sell, ms=15, color='red')plt.xlabel('Date',fontsize=14)plt.ylabel('Price in Dollars', fontsize = 14)plt.xticks(rotation='60',fontsize=12)plt.yticks(fontsize=12)plt.title('Trade Calls - Moving Averages Crossover', fontsize = 16)plt.legend(['Close','2_SMA','5_SMA'])plt.grid()plt.show() class TweetStreamListener(StreamListener):    numAnalyzed = 0    numPositive = 0    numNeutral = 0    numNegative = 0    # on success    def on_data(self, data):        # decode json        print(data)        #df = pd.DataFrame(data[0])        return True    # on failure    def on_error(self, status):        print(status)# create instance of the tweepy tweet stream listenerlistener = TweetStreamListener()# set twitter keys/tokensauth = OAuthHandler(consumer_key, consumer_secret)auth.set_access_token(access_token, access_token_secret)# instantiate apiapi = tweepy.API(auth)# create instance of the tweepy streamstream = Stream(auth, listener)# search twitter by useruser = api.get_user('realDonaldTrump')stream.filter(follow=[str(user.id)])
-=======
-import pandas as pdimport numpy as npimport requestsfrom vaderSentiment.vaderSentiment import SentimentIntensityAnalyzerimport pandas_datareader as pdrimport matplotlib.pyplot as pltdata_amd = pdr.get_data_yahoo('AMD', '1-Jan-20')data_amd.head()data_amd['2_SMA'] = data_amd['Close'].rolling(window=2).mean()data_amd['5_SMA'] = data_amd['Close'].rolling(window=5).mean()data_amd = data_amd[data_amd['5_SMA'].notna()]# SMA trade callsTrade_Buy=[]Trade_Sell=[]for i in range(len(data_amd)-1):    if ((data_amd['2_SMA'].values[i] < data_amd['5_SMA'].values[i]) & (data_amd['2_SMA'].values[i+1] > data_amd['5_SMA'].values[i+1])):        print("Trade Call for {row} is Buy.".format(row=data_amd.index[i].date()))        Trade_Buy.append(i)    elif ((data_amd['2_SMA'].values[i] > data_amd['5_SMA'].values[i]) & (data_amd['2_SMA'].values[i+1] < data_amd['5_SMA'].values[i+1])):        print("Trade Call for {row} is Sell.".format(row=data_amd.index[i].date()))        Trade_Sell.append(i)plt.figure(figsize=(20, 10),dpi=80)plt.plot(data_amd.index, data_amd['Close'])plt.plot(data_amd.index, data_amd['2_SMA'],'-^', markevery=Trade_Buy, ms=15, color='green')plt.plot(data_amd.index, data_amd['5_SMA'],'-v', markevery=Trade_Sell, ms=15, color='red')plt.xlabel('Date',fontsize=14)plt.ylabel('Price in Dollars', fontsize = 14)plt.xticks(rotation='60',fontsize=12)plt.yticks(fontsize=12)plt.title('Trade Calls - Moving Averages Crossover', fontsize = 16)plt.legend(['Close','2_SMA','5_SMA'])plt.grid()plt.show() analyser = SentimentIntensityAnalyzer()for i in range(290):    df = pd.DataFrame(requests.get('https://api.pushshift.io/reddit/search/comment/?q=tesla&subreddit=wallstreetbets&after={}d&before={}d&size=500'.format(i+1, i)))    print(df)        
->>>>>>> origin/Kwub
+
+import pandas as pd
+import numpy as np
+import requests
+import json
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+import pandas_datareader as pdr
+import matplotlib.pyplot as plt
+
+import pyrebase
+
+
+# firebase configuration keys
+firebase_config = {
+  "apiKey": "AIzaSyCmmznsEI68l0AVICMuB5Gm3rRDifJt-xE",
+  "authDomain": "sentimenttrading-f5ae7.firebaseapp.com",
+  "databaseURL": "https://sentimenttrading-f5ae7.firebaseio.com",
+  "storageBucket": "sentimenttrading-f5ae7.appspot.com",
+  "serviceAccount": "firebase_keys.json"
+}
+
+# initialize firebase
+firebase = pyrebase.initialize_app(firebase_config)
+
+# Get a reference to the auth service
+auth = firebase.auth()
+
+# Log the user in
+#user = auth.sign_in_with_email_and_password(email, password)
+
+# Get a reference to the database service
+db = firebase.database()
+
+# data to save
+data = {
+    "name": "Mortimer 'Morty' Smith"
+}
+
+# Pass the user's idToken to the push method
+results = db.child("users").push(data)
+
+data_amd = pdr.get_data_yahoo('AMD', '1-Jan-20')
+data_amd.head()
+
+data_amd['2_SMA'] = data_amd['Close'].rolling(window=2).mean()
+data_amd['5_SMA'] = data_amd['Close'].rolling(window=5).mean()
+
+data_amd = data_amd[data_amd['5_SMA'].notna()]
+
+# SMA trade calls
+Trade_Buy=[]
+Trade_Sell=[]
+for i in range(len(data_amd)-1):
+    if ((data_amd['2_SMA'].values[i] < data_amd['5_SMA'].values[i]) & (data_amd['2_SMA'].values[i+1] > data_amd['5_SMA'].values[i+1])):
+        print("Trade Call for {row} is Buy.".format(row=data_amd.index[i].date()))
+        Trade_Buy.append(i)
+    elif ((data_amd['2_SMA'].values[i] > data_amd['5_SMA'].values[i]) & (data_amd['2_SMA'].values[i+1] < data_amd['5_SMA'].values[i+1])):
+        print("Trade Call for {row} is Sell.".format(row=data_amd.index[i].date()))
+        Trade_Sell.append(i)
+
+plt.figure(figsize=(20, 10),dpi=80)
+plt.plot(data_amd.index, data_amd['Close'])
+plt.plot(data_amd.index, data_amd['2_SMA'],'-^', markevery=Trade_Buy, ms=15, color='green')
+plt.plot(data_amd.index, data_amd['5_SMA'],'-v', markevery=Trade_Sell, ms=15, color='red')
+plt.xlabel('Date',fontsize=14)
+plt.ylabel('Price in Dollars', fontsize = 14)
+plt.xticks(rotation='60',fontsize=12)
+plt.yticks(fontsize=12)
+plt.title('Trade Calls - Moving Averages Crossover', fontsize = 16)
+plt.legend(['Close','2_SMA','5_SMA'])
+plt.grid()
+plt.show() 
+
+analyser = SentimentIntensityAnalyzer()
+
+
+# for i in range(290):
+#     data = requests.get('https://api.pushshift.io/reddit/search/comment/?q=tesla&subreddit=wallstreetbets&after={}d&before={}d&size=500'.format(i+1, i))
+#     dict_data = data.json()
+#   #  df = pd.DataFrame(requests.get('https://api.pushshift.io/reddit/search/comment/?q=tesla&subreddit=wallstreetbets&after={}d&before={}d&size=500'.format(i+1, i)))
+#     print(dict_data["data"][0]["body"])
+#     #print(df)
+    
+ 
--- config.py
+++ config.py
@@ -5,9 +5,3 @@ Created on Fri Oct 16 23:40:44 2020
 
 @author: daniel
 """
-
-# list of Twitter API keys - don't modify
-consumer_key = "zUYkNEpgyrQ6smWhIjbdeZVt0"
-consumer_secret = "OoNsNZEI9lcwTh1Ie9p4f5OmAx9jTt4SflPcokpxv17jUMldTm"
-access_token = "2961040619-YR61AaVKXqXlDEESay2kYRxojTJNiQl3IkQiy1u"
-access_token_secret = "qPPDFUH36vn282dpzVCvLZ7RVeVNrhbHmANWxGNZfdrxN"
